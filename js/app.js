10c854a73c66034f4ec89ba69afac0cd52ab6e60
Ridwan Sanusi <rsanusi@Galibats-MacBook.local>
2018-03-15 23:34:17 +0100

    URL is extracted from the input field and sent to postlight api. the api returns the other details and it's stored in the localstorage aray

--- js/app.js
+++ js/app.js
@@ -19,29 +19,44 @@ function main() {
 
 
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-// When user adds a url and presses enter, the details are extracted from the
-// url gets added to an array
+// When user adds a url and presses enter, send the url to the postlight api to
+// retrieve details from the url
 
 function getArticleUrl(e) {
   if (e.keyCode == 13) {
       // Do something
-
-
-
-      storedArticleList.push({
-        url: urlInput.value
-      });
-
-      saveinStorage();
-
-      console.log(urlInput.value);
-
+			getArticleDetails();
+			console.log('processrequested');
   }
 }
 
 
-const urlInput = document.querySelector('.url-field');
-urlInput.addEventListener("keydown", getArticleUrl);
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// the details are extracted from the url and gets added to the array
+
+function getArticleDetails() {
+	var xhr = new XMLHttpRequest();
+	xhr.addEventListener("readystatechange", processRequest);
+	// var url = "https://mercury.postlight.com/parser?url="+encodeURIComponent("https://trackchanges.postlight.com/building-awesome-cms-f034344d8ed");
+	var url = "https://mercury.postlight.com/parser?url="+encodeURIComponent(urlInput.value);
+	xhr.open('GET', url, true);
+	xhr.setRequestHeader("x-api-key", "VAxVxFVwzK2q1hlKoPGdtTLpfvVdIz8Y5QwikQ7U");
+	xhr.send();
+
+	function processRequest(e) {
+		if (xhr.readyState == 4 && xhr.status == 200) {
+				// time to partay!!!
+				var response = JSON.parse(xhr.responseText);
+				console.log(response);
+				// console.log(response.title);
+
+				storedArticleList.push(response);
+				saveinStorage();
+		}
+	}
+}
 
 
 
@@ -50,7 +65,7 @@ urlInput.addEventListener("keydown", getArticleUrl);
 
 function saveinStorage() {
 	localStorage["articleList"] = JSON.stringify(storedArticleList);
-  console.log('storedArticleList Updated')
+  console.log('storedArticleList Updated');
 }
 
 
@@ -67,8 +82,14 @@ function loadfromStorage() {
 
 
 
+
 // 4. Display these urls on the page
 
+
+
+
+const urlInput = document.querySelector('.url-field');
+urlInput.addEventListener("keydown", getArticleUrl);
 
 main();
 
