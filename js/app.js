6f7cb7e69b302fdac55403a297d48b406f8ece1e
Ridwan Sanusi <rsanusi@Galibats-MacBook.local>
2018-03-16 02:47:08 +0100

    Now displays the list of articles from teh localstorage on teh frontp

--- js/app.js
+++ js/app.js
@@ -2,14 +2,14 @@
 var storedArticleList;
 
 
-
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // The Main Function
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 function main() {
 
 	if (loadfromStorage()) {
-		// displayProjectList();
+		displayArticles();
 	}
 	else {
 		storedArticleList = [];
@@ -21,6 +21,7 @@ function main() {
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // When user adds a url and presses enter, send the url to the postlight api to
 // retrieve details from the url
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 function getArticleUrl(e) {
   if (e.keyCode == 13) {
@@ -35,11 +36,12 @@ function getArticleUrl(e) {
 
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // the details are extracted from the url and gets added to the array
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 
 function getArticleDetails() {
 	var xhr = new XMLHttpRequest();
 	xhr.addEventListener("readystatechange", processRequest);
-	// var url = "https://mercury.postlight.com/parser?url="+encodeURIComponent("https://trackchanges.postlight.com/building-awesome-cms-f034344d8ed");
 	var url = "https://mercury.postlight.com/parser?url="+encodeURIComponent(urlInput.value);
 	xhr.open('GET', url, true);
 	xhr.setRequestHeader("x-api-key", "VAxVxFVwzK2q1hlKoPGdtTLpfvVdIz8Y5QwikQ7U");
@@ -62,6 +64,7 @@ function getArticleDetails() {
 
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // 3. the array is saved into the localstorage
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 function saveinStorage() {
 	localStorage["articleList"] = JSON.stringify(storedArticleList);
@@ -71,6 +74,7 @@ function saveinStorage() {
 
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // Load from storage function
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 function loadfromStorage() {
 	if(!localStorage["articleList"])
@@ -82,8 +86,33 @@ function loadfromStorage() {
 
 
 
-
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // 4. Display these urls on the page
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+function displayArticles() {
+
+	const listContainer = document.querySelector('.article-list');
+	const articleBlockTemplate = listContainer.querySelector('.article-block');
+
+	for(let article of storedArticleList) {
+			console.log(article);
+			const articleBlock = articleBlockTemplate.cloneNode(true);
+			listContainer.prepend(articleBlock);
+			const articleLink = articleBlock.querySelector('.article-link');
+			const articleAuthor = articleBlock.querySelector('.article-author');
+			const articleDomain = articleBlock.querySelector('.article-domain');
+
+			articleLink.innerText = article.title;
+			articleAuthor.innerText = 'by ' + article.author + ',';
+			articleDomain.innerText = article.domain;
+
+			console.log(listContainer);
+	}
+}
+
+
+
 
 
 
@@ -92,16 +121,3 @@ const urlInput = document.querySelector('.url-field');
 urlInput.addEventListener("keydown", getArticleUrl);
 
 main();
-
-
-//
-//
-// async function main() {
-//     var urls = ["https://trackchanges.postlight.com/building-awesome-cms-f034344d8ed", "https://medium.com/s/story/the-tech-industrys-psychological-war-on-kids-c452870464ce", "https://uxplanet.org/how-combining-schemas-and-color-psychology-will-improve-ux-13cfc70be16f", "https://www.wired.com/story/theranos-and-silicon-valleys-fake-it-till-you-make-it-culture/"];
-//     for(let url of urls) {
-//         var res = await postlight(url);
-//         console.log(res);
-//     }
-//
-// }
-//
